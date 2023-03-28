4fb414c38df4b2cec0b0261a4750081ef46ca6ca
Raúl Camacho <raulj.camacho@gmail.com>
2023-03-28 16:37:42 -0500

    ✨ feat(app.js): añadir ejemplos de variables y asignaciones en consola

--- 01-Fundamentos/app.js
+++ 01-Fundamentos/app.js
@@ -1,1 +1,18 @@
-console.log('Hola Mundo');
+// Curso de JavaScrip Fundamentos
+
+// console.log('Hola Mundo');
+
+// Variables
+
+let a = 10;
+var b = 10;
+const c = 10;
+
+console.log(a);
+
+// c = 20; Esto no se puede hacer
+
+a = 20;
+b = 20;
+
+console.log(a);
