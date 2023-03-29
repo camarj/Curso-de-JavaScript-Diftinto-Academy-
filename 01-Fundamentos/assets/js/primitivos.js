0caddf5d31dccf055151c27c19bde0ea92c974bd
Raúl Camacho <raulj.camacho@gmail.com>
2023-03-29 16:49:21 -0500

    📦 chore(primitivos.js): añadir archivo primitivos.js con ejemplos de tipos primitivos 🔥 refactor(index.html): comentar importación de alerts.js y app.js y añadir importación de primitivos.js

---
+++ 01-Fundamentos/assets/js/primitivos.js
@@ -0,0 +1,28 @@
+// Tipos primitivos
+
+// String
+const nombre = 'Luiz';
+console.log(typeof nombre);
+
+// Number
+const edad = 30;
+console.log(typeof edad);
+
+// Boolean
+const isRegister = true;
+console.log(typeof isRegister);
+
+// Simbols  (ES6)
+const simbolo1 = Symbol();
+const simbolo2 = Symbol();
+console.log(typeof simbolo1);
+
+console.log(simbolo1 === simbolo2);
+
+// Undefined
+let nombre2;
+console.log(typeof nombre2);
+
+// Null
+let nombre3 = null;
+console.log(typeof nombre3);
