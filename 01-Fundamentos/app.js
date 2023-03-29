7781bedfa22cb72d97101ddbefee9b68df6dc227
Raúl Camacho <raulj.camacho@gmail.com>
2023-03-29 13:38:29 -0500

    ✨ feat(app.js): añadir comentarios explicando el uso de let, var y const 🔥 chore(app.js): eliminar código comentado y no utilizado ✨ feat(app.js): añadir ejemplos de uso de console.log y console.table ✨ feat(app.js): añadir ejemplo de concatenación de strings con variables

--- 01-Fundamentos/app.js
+++ 01-Fundamentos/app.js
@@ -4,25 +4,30 @@
 
 // Variables
 
-let a = 10;
-var b = 10;
-const c = 10;
+// let a = 10;
+// var b = 10;
+// const c = 10;
 
-console.log(a);
+// console.log(a);
 
 // c = 20; Esto no se puede hacer
 
-a = 20;
-b = 20;
+// a = 20;
+// b = 20;
 
-console.log(a);
+// console.log(a);
 
 // Explicar el uso de let, var y const y por que no usar var
 
-let d = 10;
-let f = 10;
-let g = 10;
-let h = d + f;
+// let d = 10;
+// let f = 10;
+// let g = 10;
+// let h = d + f;
+
+// let d = 10;
+// let f = 'Hola Mundo';
+// let g = 'Metallica';
+// let h = d + f;
 
 // Esto es otra manera de declarar variables en js pero es menos legible
 // let d = 10, f = 10, g = 10, h = d + f;
@@ -31,4 +36,31 @@ let h = d + f;
 //     g = 10,
 //     h = d + f;
 
-console.log(h);
+// console.log(h);
+
+// Uso del console.log
+
+// console.log('Hola Mundo');
+// console.log(20);
+// console.log(true);
+
+// console.log('d', d);
+// console.log('f', f);
+// console.log('g', g);
+
+// Mostrarlo como objetos y agregarles un color y un estilo
+// console.log('%c Mis variables', 'color: green; font-weight: bold');
+// console.log({ d });
+// console.log({ f });
+// console.log({ g });
+
+// Mostrar un log en tabla
+
+// console.table({ d, f, g });
+
+let a = 'Hola ';
+let b = 'Metallica';
+
+const saludo = a + b;
+
+console.log(saludo);
